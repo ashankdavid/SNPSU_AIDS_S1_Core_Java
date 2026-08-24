5bbf0d3fa4842ecbb506437acd6b48b8af4754e5
ashankdavid <ashank2507david@gmail.com>
2026-08-24 16:10:16 +0530

    finished Exceptions and started with Threads...continued in next class...

---
+++ src/com/snpsu/ExceptionHandling/E1.java
@@ -0,0 +1,23 @@
+package com.snpsu.ExceptionHandling;
+
+public class E1 {
+    public static void main(String[] args) {
+        int i, j, k=0;
+        i= 10;
+        j= 2;
+        try{
+            k=i/j;
+            int[] arr = {1,2,3,4,5};
+            System.out.println(arr[1]);
+        }catch (ArithmeticException e){
+            System.out.println("Cant Divide by Zero");
+        }catch (Exception e){
+            System.out.println("Unknown Exception Found!");
+        }finally{
+            System.out.println("This will Execute no matter what!!");
+        }
+        System.out.println("Hello");
+    }static{
+        System.out.println("Execution Starts");
+    }
+}
---
+++ src/com/snpsu/ExceptionHandling/InvalidAgeException.java
@@ -0,0 +1,17 @@
+package com.snpsu.ExceptionHandling;
+
+public class InvalidAgeException extends Exception{
+    InvalidAgeException(String msg){
+        super(msg);
+    }
+}
+
+class Driver{
+    public static void main(String[] args) throws InvalidAgeException {
+        int age = 10;
+        if(age<18){
+            throw new InvalidAgeException("Age must be over 18!");
+        }
+        System.out.println("You can vote!");
+    }
+}
